3e071e0c93f9282ba300eff8641dcb3b8083bdc7
John D. Ament <johndament@apache.org>
2016-12-31 12:30:17 -0500

    Creating build script.

---
+++ build_site.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+rm -rf /tmp/incubator-site
+ant docs -Ddocs.dest=/tmp/incubator-site
+git checkout asf-site
+git clean -f -d
+git pull origin asf-site
+cp -a /tmp/incubator-site/. output/
+git add .
+#git commit -m "Automatic Site Publish by Buildbot"
+#git push origin asf-site
