d5c0224efaf540fcef3643e1b04ab376361a248f
John D. Ament <johndament@apache.org>
2017-04-17 21:54:06 -0400

    Change how generation works.

--- build_site.sh
+++ build_site.sh
@@ -8,7 +8,7 @@ git checkout asf-site
 git clean -f -d
 git pull origin asf-site
 rm -rf *
-cp -a /tmp/incubator-site/. .
+cp -a /tmp/incubator-site/* .
 git add .
 git commit -m "Automatic Site Publish by Buildbot"
 git push origin asf-site
