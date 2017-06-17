614634b930ffb6a904e49d6b07ba862fe4c6295b
John D. Ament <johndament@apache.org>
2017-06-16 20:03:21 -0400

    Setup output to working directory.

--- build_site.sh
+++ build_site.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 
-rm -rf /tmp/incubator-site
-$JBAKE_HOME/bin/jbake -b
-mkdir /tmp/incubator-site
-cp -R build/jbake/* /tmp/incubator-site
+WORKDIR=/tmp/incubator-site
+rm -rf $WORKDIR
+mkdir -p $WORKDIR
+$JBAKE_HOME/bin/jbake -b . $WORKDIR
 git checkout asf-site
 git clean -f -d
 git pull origin asf-site
 rm -rf *
-cp -a /tmp/incubator-site/* .
+cp -a $WORKDIR/* .
 git add .
 git commit -m "Automatic Site Publish by Buildbot"
 git push origin asf-site
