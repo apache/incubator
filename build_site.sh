7a95a7d6da8d03793a8fe04c89e2c37f41882554
Dave Fisher <dave@davefisher.tech>
2019-03-18 17:30:17 -0700

    Build with new clutch process and pages
    Build is reordered and uses a new clutch process. Site is baked after the SVN, Anakia, and Clutch2 are run. If clutch2 fails then the process should abort

--- build_site.sh
+++ build_site.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+CURRENTDIR=`pwd`
 WORKDIR=/tmp/incubator-site
 SVN_CO_DIR=/tmp/incubator-site-content
 SVN_BUILD_DIR=/tmp/incubator-site-build
@@ -8,7 +9,9 @@ SVN_REPO=http://svn.apache.org/repos/asf/incubator/public/trunk/
 # build the git bits
 rm -rf $WORKDIR
 mkdir -p $WORKDIR
-./bake.sh -b . $WORKDIR
+mkdir -p $WORKDIR/clutch
+
+echo $CURRENTDIR
 
 # build the svn bits
 rm -rf $SVN_CO_DIR
@@ -17,11 +20,24 @@ svn co $SVN_REPO $SVN_CO_DIR
 (
     cd $SVN_CO_DIR
     ant docs -Ddocs.dest=$SVN_BUILD_DIR
-    python3 clutch.py
+    # these files were built using ant in the old style
     mv $SVN_BUILD_DIR/ip-clearance $WORKDIR
     mv $SVN_BUILD_DIR/projects $WORKDIR
-    mv $SVN_BUILD_DIR/clutch $WORKDIR
+    # new clutch2 process
+    ./clutch2.sh
+    if [ $? -gt 0 ]; then
+	echo clutch2.sh returned $? - ABORT
+	exit 4
+    fi
+    # these txt files are moved to output
+    cp $SVN_CO_DIR/content/clutch/*.txt $WORKDIR/clutch/.
+    # the following files go to be baked
+    cp $SVN_CO_DIR/content/clutch/_includes/*.ad $CURRENTDIR/pages/clutch/_includes/.
+    cp $SVN_CO_DIR/content/clutch/*.ad $CURRENTDIR/pages/clutch/.
 )
+
+# now bake the site
+./bake.sh -b . $WORKDIR
 
 # push all of the results to asf-site
 git checkout asf-site
