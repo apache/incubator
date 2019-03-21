7b896244fbcf8adafaa12f4378eebdbc4cadb99d
Dave Fisher <dave@davefisher.tech>
2019-03-21 11:53:32 -0700

    Properly invoke jbake 2.6.x

--- bake.sh
+++ bake.sh
@@ -1,5 +1,4 @@
 #!/bin/sh
 
-#export JBAKE_OPTS="-Djavax.xml.accessExternalDTD=http"
-#jbake $@
-java -Djavax.xml.accessExternalDTD=http -jar "${JBAKE_HOME}/jbake-core.jar" "$@"
+export JBAKE_OPTS="-Djavax.xml.accessExternalDTD=http"
+${JBAKE_HOME}/bin/jbake $@
