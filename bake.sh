507a918a1dae74c22671008e1d65c9f577f234c3
Dave Fisher <dave@davefisher.tech>
2019-03-21 09:00:05 -0700

    Fix breaking change

--- bake.sh
+++ bake.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
 
-export JBAKE_OPTS="-Djavax.xml.accessExternalDTD=http"
-jbake $@
+#export JBAKE_OPTS="-Djavax.xml.accessExternalDTD=http"
+#jbake $@
+java -Djavax.xml.accessExternalDTD=http -jar "${JBAKE_HOME}/jbake-core.jar" "$@"
