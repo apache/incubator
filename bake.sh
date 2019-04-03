c3cb8a588ccafc020dd2c71a22a61f4e3c1c956a
Bertrand Delacretaz <bdelacretaz@apache.org>
2019-04-03 10:49:18 +0200

    Check for JBAKE_HOME

--- bake.sh
+++ bake.sh
@@ -1,4 +1,10 @@
 #!/bin/sh
 
+if [[ -z ${JBAKE_HOME} ]]
+then
+  echo "Missing JBAKE_HOME variable" >&2
+  exit 1
+fi
+
 export JBAKE_OPTS="-Djavax.xml.accessExternalDTD=http"
 ${JBAKE_HOME}/bin/jbake $@
