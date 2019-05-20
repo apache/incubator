c1456b4a1efb0724ab8a4a6b2a4cd3ecc464b4af
Bertrand Delacretaz <bdelacretaz@apache.org>
2019-05-20 13:48:40 +0200

    Add cat -s

--- preprocess_content.sh
+++ preprocess_content.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
  
-echo "Preprocessing the Board report content" 
+IN=report.txt
+OUT=report-processed.txt
 
 # Use fold to wrap lines within 76 chars
+# cat -s to get single-space output
 # And sed to escape HTML characters
-fold -s -w 76 < report.txt | sed 's/&/\&amp;/g; s/</\&lt;/g; s/>/\&gt;/g; s/"/\&quot;/g; s/'"'"'/\&#39;/g' > report-processed.txt
+echo "Preprocessing the Board report content, $IN -> $OUT ..."
+fold -s -w 76 < $IN | cat -s | sed 's/&/\&amp;/g; s/</\&lt;/g; s/>/\&gt;/g; s/"/\&quot;/g; s/'"'"'/\&#39;/g' > $OUT
