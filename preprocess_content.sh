7d6a737e9d146ec647c562ff21598a2535396459
Bertrand Delacretaz <bdelacretaz@apache.org>
2019-05-20 13:06:42 +0200

    Explain what the script does

--- preprocess_content.sh
+++ preprocess_content.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
  
 echo "Preprocessing the Board report content" 
+
+# Use fold to wrap lines within 76 chars
+# And sed to escape HTML characters
 fold -s -w 76 < report.txt | sed 's/&/\&amp;/g; s/</\&lt;/g; s/>/\&gt;/g; s/"/\&quot;/g; s/'"'"'/\&#39;/g' > report-processed.txt
