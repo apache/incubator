449b76645c91d020c4f2f69b15783d978f3dfbca
Sebb <sebb@apache.org>
2024-05-19 23:40:33 +0100

    Allow for serve and/or bake using Docker

--- docker_build.sh
+++ docker_build.sh
@@ -1,15 +1,43 @@
 #!/bin/bash
 
-# Build the site locally using the JBake Docker image
+# Build and/or serve the site locally using the JBake Docker image
 
 function fatal() {
     echo $* >&2
     exit 1
 }
 
-# just bake the site
-docker run --rm \
-    -u jbake \
-    -v "$PWD":/mnt/site \
-    -e 'JBAKE_OPTS="-Djavax.xml.accessExternalDTD=http,https"' \
-    jbake/jbake:latest || fatal "Build failed, exiting"
+# N.B. jbake-docker.properties defines server.hostname=0.0.0.0
+# This is necessary to ensure the server accepts external requests
+# (The documentation says to use localhost, but that does not work)
+
+case "$1" in
+    -s)
+    # just serve the site
+    docker run --rm \
+        -u jbake \
+        -v "$PWD":/mnt/site \
+        -e 'JBAKE_OPTS="-Djavax.xml.accessExternalDTD=http,https"' \
+        -p '8820:8820' \
+        jbake/jbake:latest -c jbake-docker.properties -s || fatal "Build failed, exiting"
+    ;;
+    -bs)
+    # bake and serve the site
+    docker run --rm \
+        -u jbake \
+        -v "$PWD":/mnt/site \
+        -e 'JBAKE_OPTS="-Djavax.xml.accessExternalDTD=http,https"' \
+        -p '8820:8820' \
+        jbake/jbake:latest -c jbake-docker.properties -b -s || fatal "Build failed, exiting"
+    ;;
+    -b)
+    # just bake the site
+    docker run --rm \
+        -u jbake \
+        -v "$PWD":/mnt/site \
+        -e 'JBAKE_OPTS="-Djavax.xml.accessExternalDTD=http,https"' \
+        jbake/jbake:latest || fatal "Build failed, exiting"
+    ;;
+    *) echo "Valid options are: -b (bake), -s (serve), -bs (bake and serve)"
+    ;;
+esac
